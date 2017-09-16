4bc14142f309793412feb6b4d2a27a96625bf861
visha12l <vsvshinde15@gmail.com>
2017-09-16 14:09:38 +0530

    adds code for annalog clock

---
+++ src/javascript/containers/clockIndex.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ClockIndex from '../analogClock/index.js';
+
+const Index = () =>  {
+    return (
+        <div className='ChildElement'>
+            <ClockIndex />
+        </div>
+    );
+};
+export default Index;
